57b76395d6549cbb596cfab896acdb72f3b52a06
liuceyi <602483575@qq.com>
2020-07-07 12:54:49 +0800

    restore bug in skillJson

--- Assets/Scripts/Game/UI/Menu/MenuSkill/MenuSkillManager.cs
+++ Assets/Scripts/Game/UI/Menu/MenuSkill/MenuSkillManager.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class MenuSkillManager : MenuChildManager
 {
-
+    private List<Skill> skillList;
+    public GameObject itemSkillPrefab;
+    public RectTransform skillContent;
     public override void Init()
     {
         Debug.Log("InitMenuSkillManager");
+        skillList = ((SkillUser)JsonController.Instance.getJsonObj("skill")).Character01;
+        spawnItemSkill();
+    }
+    void spawnItemSkill() 
+    {            
+        GameObject itemSkillUnit = Instantiate(itemSkillPrefab, skillContent);
+        itemSkillUnit.transform.position = new Vector2(0,0);
+        
+       
     }
 
 }
